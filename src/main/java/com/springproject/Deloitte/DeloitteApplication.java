6250846336dae6f4f6137b1cf4d84523cbf5af59
Muhamed Sabic <m.sabic2017@gmail.com>
2021-01-12 16:22:31 +0000

    ProjectCommit

---
+++ src/main/java/com/springproject/Deloitte/DeloitteApplication.java
@@ -0,0 +1,14 @@
+package com.springproject.Deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DeloitteApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeloitteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springproject/Deloitte/DeloitteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.Deloitte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeloitteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
